2e48f4855f066ebb9e6bfec8885392ad0fdf9ec2
Mikk Laur <mikklaur5@gmail.com>
2021-12-12 23:56:44 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import java.time.Duration;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            driver.get("https://ansokan.bigbank.se/?amount=85000&period=120&interestRate=10.95&lang=sv&bbsource=google&bbchannel=organic");
+            driver.findElement(By.cssSelector(".bb-button__icon-after")).click();
+            driver.findElement(By.cssSelector(".bb-calculator__result-value"));
+        } finally {
+            //driver.quit();
+        }
+    }
+}
+  
---
+++ test/main/java/MainTest.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
